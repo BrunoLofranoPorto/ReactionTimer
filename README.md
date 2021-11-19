ff181a574ee18df6c6f026dc42baac493d770001
BrunoLofranoPorto <89394173+BrunoLofranoPorto@users.noreply.github.com>
2021-11-19 10:03:54 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ReactionTimer
+This is a reaction time game created while taking the vue3 course by TheNetNinja on youtube.
+It is the first vue project i have ever created and i intend on developing it in the future.
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>You took {{ score }} ms to click the box!</h1>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'E-Sports level'
+        } else if (this.score < 400) {
+            this.rank = 'Casual gamer'
+        } else {
+            this.rank = 'Grandpa?'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.7em;
+        color: #251857;
+        font-weight: bold;
+    }
+</style>
